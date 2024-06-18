4ceb430e49c59bc11ade055dcc590643cc4dfb8f
cadenweigel <157074200+cadenweigel@users.noreply.github.com>
2024-06-17 17:04:06 -0700

    added some functions

--- parsers/spotify.py
+++ parsers/spotify.py
@@ -1,39 +1,20 @@
 import json
 from typing import List
 import stream_parser
-import spotify_objs
+import misc_parsers as parser
+import spotify_objs as sp
 
 """
 Main file for parsing spotify data
 End goal is to have this program format the data in a more accessible way
 """
 
-#should probably move this function elsewhere
-def parse_marquee():
-    """
-    Returns a dictionary where the keys are segments
-    and the values are the number of artists in that segment
-    """
-
-    f = 'SpotifyAccountData/Marquee.json'
-    with open(f, 'r', encoding="utf8", errors="replace") as file:
-        file_content = file.read()
- 
-    data = json.loads(file_content) #creates a list of dicts
-
-    segments = {} #track the possible segment options
-    for i in data:
-        seg = i['segment']
-        if seg not in segments:
-            segments[seg] = 1
-        else:
-            segments[seg] += 1
-
-    return segments
-
 def main():
-    segments = parse_marquee()
+    segments = parser.parse_marquee()
     streams = stream_parser.getStreams()
+    artists = parser.get_segment_artists("Super Listeners")
+    a = sp.Artist("jim")
+
 
 if __name__ == "__main__":
     main()
--- parsers/spotify_objs.py
+++ parsers/spotify_objs.py
@@ -3,10 +3,14 @@ from typing import List
 class ArtistList():
     def __init__(self) -> None:
         self.artists: List[Artist] = []
+    def __str__(self):
+        return self.artists
 
 class SongList():
     def __init__(self) -> None:
         self.songs: List[Song] = []
+    def __str__(self):
+        return self.songs
 
 class Artist():
     """
@@ -17,6 +21,9 @@ class Artist():
     def __init__(self, name: str) -> None:
         self.songs: List[Song] = []
         self.name = name
+
+    def __str__(self):
+        return self.name
 
 class Song():
     """
@@ -30,6 +37,12 @@ class Song():
         self.streams = 1 #initialized to 1 since it's initialized the first time it's found
         self.listenTime = ms
 
+    def __str__(self):
+        return self.name #FIXME: rework to give name, artist, streams
+
     def addStream(self, ms):
         self.streams += 1
         self.listenTime += ms
+
+    def estimate_song_length(self):
+        return self.listenTime / self.streams #FIXME: rework to return in time format?
